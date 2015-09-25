06031e992f2f047246e3cf330bc57a1a3604cd39
Babatunde Fashola <bfashola@Deborahs-MacBook-Pro.local>
2015-09-24 23:34:02 -0400

    Created the NaijaMascotContrct Class...

--- app/src/main/java/com/naijamascot/NaijaMascotContract.java
+++ app/src/main/java/com/naijamascot/NaijaMascotContract.java
@@ -1,7 +1,43 @@
 package com.naijamascot;
 
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 /**
  * Created by bfashola on 9/22/15.
  */
 public class NaijaMascotContract {
+    interface NaijaMascotColumns{
+        String NAIJAMASCOT_FIRSTNAME = "Firstname";
+        String NAIJAMASCOT_LASTNAME = "Lastname";
+        String NAIJAMASCOT_HINT = "Hint";
+        String NAIJAMASCOT_ANSWER = "Answer";
+        String NAIJAMASCOT_CATEGORY = "Category";
+        String NAIJAMASCOT_IMAGE = "Image";
+    }
+
+    public static final String CONTENT_AUTHORITY = "com.naijamascot.provider";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    private static final String PATH_NAIJAMASCOT  = "NaijaMascots";
+
+    public static final String [] TOP_LEVEL_PATHS = {
+            PATH_NAIJAMASCOT
+    };
+
+    private static class NaijaMascts implements NaijaMascotColumns, BaseColumns {
+        public static final Uri URI_CONTENT =
+                BASE_CONTENT_URI.buildUpon().appendEncodedPath(PATH_NAIJAMASCOT).build();
+
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.com.naijamacsot" + PATH_NAIJAMASCOT;
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.com.naijamacsot" + PATH_NAIJAMASCOT;
+
+        public static Uri buildNaijaMascotUri(String Id){
+            return URI_CONTENT.buildUpon().appendEncodedPath(Id).build();
+        }
+        public static String getNaijaMascotId(Uri uri){
+            return uri.getPathSegments().get(1);
+        }
+
+    }
 }
--- app/src/main/java/com/naijamascot/NaijaMascotDatabase.java
+++ app/src/main/java/com/naijamascot/NaijaMascotDatabase.java
@@ -1,6 +1,7 @@
 package com.naijamascot;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
@@ -12,12 +13,22 @@ public class NaijaMascotDatabase extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 2;
     private final Context mContext;
 
-    interface Table {
-        String NAIJAMASCOTS = "NaijaMascots";
-    }
-
     public NaijaMascotDatabase(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    interface Table {
+        String NAIJAMASCOTS = "NaijaMascots";
     }
 }
